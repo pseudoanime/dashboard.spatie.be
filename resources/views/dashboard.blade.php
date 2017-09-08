88c254878810cc94176538d9e0e834212dbf5d2b
pseudoanime <luckilaxmi@gmail.com>
2017-09-08 01:45:04 +0100

    updating

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,14 @@
     <music position="c1:d1"></music>
     <uptime position="b3"></uptime>
 
-    <tasks team-member="alex" position="c2"></tasks>
-    <tasks team-member="freek" position="d2"></tasks>
-    <tasks team-member="seb" position="c3"></tasks>
-    <tasks team-member="willem" position="d3"></tasks>
+    <tasks team-member="Jermaine" position="c2"></tasks>
+    <tasks team-member="Lakshmi" position="d2"></tasks>
+    <tasks team-member="Sam" position="c3"></tasks>
+    <tasks team-member="Vanessa" position="d3"></tasks>
+    <tasks team-member="Mariusz" position="e2"></tasks>
 
     <time-weather position="e1" date-format="ddd DD/MM"></time-weather>
-    <packagist position="e2"></packagist>
+    <!-- <packagist position="e2"></packagist> -->
     <github position="e3"></github>
 
     <internet-connection></internet-connection>
